4b0d3484890adcc024601339d7f36547170ccbcc
hugoclaude <hugocayeros@gmail.com>
2020-12-14 06:58:42 -0800

    added no-content to post links when post links are empty for post, and applied flex styles to post links

--- src/components/post.js
+++ src/components/post.js
@@ -21,24 +21,24 @@ class Post extends Component {
     }
 
     getNameForPostLink(str) {
-        var n = str.lastIndexOf('/');
+        var n = str.lastIndexOf("/");
         var link = str.substring(n + 1, str.length);
 
-        if((n+1) == str.length) {
+        if (n + 1 == str.length) {
             link = str.slice(0, n);
-            n = link.lastIndexOf('/');
+            n = link.lastIndexOf("/");
             link = str.substring(n + 1, str.length - 1);
-        }    
+        }
 
-        if(link.includes('.html')) {
+        if (link.includes(".html")) {
             link = link.substring(0, link.length - 5);
         }
-        if(link.includes('.htm')) {
+        if (link.includes(".htm")) {
             link = link.substring(0, link.length - 4);
         }
 
         return link;
-    }   
+    }
 
     renderLinks() {
         let links = this.props.post_links.map((post_link, index) => {
@@ -46,12 +46,16 @@ class Post extends Component {
                 <div className="post-link" key={index}>
                     <div className="post-link__box"></div>
                     <div className="post-link__link">
-                        <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}
+                        <a href={post_link.link_url}>
+                            {this.getNameForPostLink(post_link.link_url)}
                         </a>
                     </div>
                 </div>
             );
         });
+        if (links == 0) {
+            return <div className="no-content">No Post Links</div>;
+        }
         return links;
     }
 
@@ -69,7 +73,8 @@ class Post extends Component {
             );
         } else if (this.props.type == "result") {
             return (
-                <li className="result-post"
+                <li
+                    className="result-post"
                     onMouseEnter={() => this.setState({ height: 70 })}
                     onMouseLeave={() => this.setState({ height: 0 })}
                 >
@@ -77,12 +82,9 @@ class Post extends Component {
                         {this.renderTopics()}
                     </div>
                     <div className="result-post__title">
-                        <a href={this.props.url_for_post}>
-                        {this.props.title}
-                        </a>
+                        <a href={this.props.url_for_post}>{this.props.title}</a>
                     </div>
-                    
-                    
+
                     <AnimateHeight duration={500} height={this.state.height}>
                         <div className="result-post__links">
                             {this.renderLinks()}
